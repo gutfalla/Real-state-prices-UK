6b8e5c353a24774de1c2d79e894a6ed703a59dd4
gutfalla <155889757+gutfalla@users.noreply.github.com>
2024-03-27 21:07:04 +0000

    thing

--- real_state_prices_uk/transformers/transform_ppd2023.r
+++ real_state_prices_uk/transformers/transform_ppd2023.r
@@ -1,4 +1,5 @@
 transform <- function(df_1, ...) {
+    
     # Count NA values
     na_count <- colSums(is.na(df_1))
     print("NA counts before elimination:")
@@ -12,5 +13,18 @@ transform <- function(df_1, ...) {
     print("NA counts after elimination:")
     print(na_count_after)
 
+    # Declare variables
+
+    df_1$Date <- as.Date(df_1$Date)
+
+    # Check if the column is of Date class
+    if ("Date" %in% class(df_1$Date)) {
+      print("Column is in date format")
+    } else {
+      print("Column is not in date format")
+    }
+
+    str(df_1)
     df_1
+    
 }
