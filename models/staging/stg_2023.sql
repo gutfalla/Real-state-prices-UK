19e383f7564a05907d8f440e42946268219bf9de
HD Falla <hdfallag@gmail.com>
2024-04-01 16:28:59 +0000

    V1

--- models/staging/stg_2023.sql
+++ models/staging/stg_2023.sql
@@ -11,14 +11,14 @@ renamed as (
     select
         ID,
         Price,
-        Selling_date as Selling date,
+        Selling_date,
         Postcode,
         Kind,
-        {{ desc_kind(Kind) }} as Estate kind,
+        {{ desc_kind(Kind) }} as Estate_kind,
         Condition,
-        {{ desc_condition(condition) }} as Estate condition,
-        Estate_type as Estate type,
-        {{ desc_duration(Estate_type) }} as Estate duration,
+        {{ desc_condition(condition) }} as Estate_condition,
+        Estate_type,
+        {{ desc_duration(Estate_type) }} as Estate_duration,
         PAON,
         SAON,
         Street,
@@ -27,9 +27,9 @@ renamed as (
         District,
         County,
         A,
-        {{ desc_a(A) }} as Price paid,
+        {{ desc_a(A) }} as Price_paid,
         A1,
-        {{ desc_a1(A1) }} as Record status
+        {{ desc_a1(A1) }} as Record_status
 
     from source
 
